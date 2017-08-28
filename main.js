2c17bfff06314798e08842b293dde7ef972441b0
Jacqueline Linang <Jacque3L@verizon.net>
2017-08-27 22:34:36 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+$( document ).ready(function() {
+    AOS.init();
+    var logoLeft = document.querySelector('#animateLeft');
+    var logoRight = document.querySelector('#animateRight');
+    drawSVG(logoLeft);
+    drawSVG(logoRight);
+    drawSVG(glowLeft);
+    drawSVG(glowRight);
+});
+
+function drawSVG(pathID){
+  var path = pathID;
+  var length = path.getTotalLength();
+  // Clear any previous transition
+  path.style.transition = path.style.WebkitTransition =
+    'none';
+  // Set up the starting positions
+  path.style.strokeDasharray = length + ' ' + length;
+  path.style.strokeDashoffset = length;
+  // Trigger a layout so styles are calculated & the browser
+  // picks up the starting position before animating
+  path.getBoundingClientRect();
+  // Define our transition
+  path.style.WebkitTransition = 'stroke-dashoffset 3s ease-in-out';
+  // Go!
+  path.style.strokeDashoffset = '0';
+}
+
+function switchSVG(path)
+{
+    // $("#animate").hide();
+    // $("#glow").hide();
+    $("#JL").show();
+    // $("#logoSVG").attr("data-aos","fade-up");
+}
