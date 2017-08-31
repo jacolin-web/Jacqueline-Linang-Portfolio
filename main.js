ce8c7a00c07936d3fa7ab2246f7108b5bd284789
Jacqueline Linang <Jacque3L@verizon.net>
2017-08-30 23:39:01 -0400

    alpha 2.4
    Icons change fill on load :)

--- main.js
+++ main.js
@@ -7,9 +7,29 @@ $( document ).ready(function() {
     drawSVG(logoRight);
     drawSVG(glowLeft);
     drawSVG(glowRight);
-    svgFileToInline();
+
 });
 
+$(window).on( "load", function(){
+  // Get the SVG document inside the Object tag
+  updateFill();
+
+});
+
+function updateFill() {
+  var navIDs = ["aboutNav", "workNav","bookNav", "activityNav","contactNav"];
+  for(var i = 0 ; i < navIDs.length; i++){
+      var a = document.getElementById(navIDs[i]);
+      var svgDoc = a.contentDocument;
+      // console.log(svgDoc);
+    	// Get one of the SVG items by ID;
+    	var svgItem = svgDoc.querySelector("svg");
+    	// Set the colour to something else
+      // svgItem.each(function(){
+        svgItem.style.fill = "#fff";
+      // });
+  }//end of for loops
+}
 function drawSVG(pathID){
   var path = pathID;
   var length = path.getTotalLength();
@@ -44,43 +64,4 @@ function hideStart(){
         console.log("I'm gonna do it!")
         $('#aboutSVG').addClass('animated pulse');
     }
-}
-
-function svgFileToInline(){
-    /*
-     * Replace all SVG images with inline SVG
-     */
-    jQuery('img.navLogos').each(function(){
-        var $img = jQuery(this);
-        var imgID = $img.attr('id');
-        var imgClass = $img.attr('class');
-        var imgURL = $img.attr('src');
-
-        jQuery.get(imgURL, function(data) {
-            // Get the SVG tag, ignore the rest
-            var $svg = jQuery(data).find('svg');
-
-            // Add replaced image's ID to the new SVG
-            if(typeof imgID !== 'undefined') {
-                $svg = $svg.attr('id', imgID);
-            }
-            // Add replaced image's classes to the new SVG
-            if(typeof imgClass !== 'undefined') {
-                $svg = $svg.attr('class', imgClass+' replaced-svg');
-            }
-
-            // Remove any invalid XML tags as per http://validator.w3.org
-            $svg = $svg.removeAttr('xmlns:a');
-
-            // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
-            if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
-                $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
-            }
-
-            // Replace image with new SVG
-            $img.replaceWith($svg);
-
-        }, 'xml');
-
-    });
 }
